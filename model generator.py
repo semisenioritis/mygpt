015df4d22fbef6cbd33728b259cc046304793789
semisenioritis <89476245+semisenioritis@users.noreply.github.com>
2023-04-13 11:13:12 +0530

    Update model generator.py

--- model generator.py
+++ model generator.py
@@ -16,9 +16,9 @@ n_layer = 6
 dropout = 0.4
 # ------------
 
-torch.manual_seed(555)
+torch.manual_seed(1337)
 
-with open('taylorswift.txt', 'r', encoding='utf-8') as f:
+with open('edsheeran.txt', 'r', encoding='utf-8') as f:
     text = f.read()
 
 # here are all the unique characters that occur in this text
@@ -182,16 +182,33 @@ class BigramLanguageModel(nn.Module):
             idx = torch.cat((idx, idx_next), dim=1) # (B, T+1)
         return idx
 
-
+model = BigramLanguageModel()
+m = model.to(device)
+# print the number of parameters in the model
+print(sum(p.numel() for p in m.parameters())/1e6, 'M parameters')
 
 # create a PyTorch optimizer
 optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
 
+for iter in range(max_iters):
 
+    # every once in a while evaluate the loss on train and val sets
+    if iter % eval_interval == 0 or iter == max_iters - 1:
+        losses = estimate_loss()
+        print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+
+    # sample a batch of data
+    xb, yb = get_batch('train')
+
+    # evaluate the loss
+    logits, loss = model(xb, yb)
+    optimizer.zero_grad(set_to_none=True)
+    loss.backward()
+    optimizer.step()
 
 # generate from the model 
 context = torch.zeros((1, 1), dtype=torch.long, device=device)
-
+print(decode(m.generate(context, max_new_tokens=2000)[0].tolist()))
 
 
 if torch.cuda.is_available():
